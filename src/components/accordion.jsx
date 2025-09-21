9534bd6c43c06c8b4b08d8e4057066c8f2f5594b
vivektarun <vivektarun1234@gmail.com>
2025-09-22 00:20:51 +0530

    feat: make accordion using one file and static data in app.js

--- src/components/accordion.jsx
+++ src/components/accordion.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa'
+import './style.css'
+
 function Accordion({items}) {
+    const [openIndex, setOpenIndex] = useState(null);
+
+    function toggleOpen(index) {
+        setOpenIndex(openIndex === index ? null : index)
+    }
     return (
-        <>
-            <h1>Accordion</h1>
-        </>
+        <div className='accordion'>
+            {items.map((item, index) => {
+                return (
+                    <div className='accordion-item'>
+                        <button className='accordion-title' key={index} onClick={() => {toggleOpen(index)}}>
+                            {item.title}
+                            {openIndex === index ? <FaChevronDown className='right'/> : <FaChevronUp className='right'/>}
+                        </button>
+                        { openIndex === index && <div className='accordion-content' key = {index}> // here we are short-circuiting 
+                            {item.content}
+                        </div> }
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
